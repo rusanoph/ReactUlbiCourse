6a9e4556962b4e4f7bd6d4a67d3fc97a05a7f589
Maxim Rusanov <rusanoph@id-r.ru>
2024-02-28 17:32:14 +0300

    useEffect to load Posts from JsonPlaceholder on startup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useMemo } from "react";
+import React, { useState, useRef, useMemo, useEffect } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
 import './styles/App.css';
@@ -8,6 +8,7 @@ import PostFilter from "./components/PostFilter";
 import Modal from "./components/UI/modal/Modal";
 import Button from "./components/UI/button/Button";
 import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
 
 
 function App() {
@@ -16,11 +17,20 @@ function App() {
 	const [modal, setModal] = useState(false);
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+	useEffect(() => { 
+		fetchPosts();
+	}, []);
+
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost])
 		setModal(false)
 	}
 	const removePost = (post) => setPosts(posts.filter(p => p.id !== post.id))
+
+	async function fetchPosts() {
+		const response = await axios.get(`https://jsonplaceholder.typicode.com/posts`);
+		setPosts(response.data);
+	}
 
 	return (
 		<div className="App">
@@ -32,7 +42,9 @@ function App() {
 			</Modal>
 
 			<PostFilter filter={filter} setFilter={setFilter} />
+			
 			<hr style={{ margin: '15px 0' }} />
+
 			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post List" />
 
 		</div>
