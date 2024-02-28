4fcf9a0a87a4d880f798461ea65952683c6e6315
Maxim Rusanov <rusanoph@id-r.ru>
2024-02-28 17:11:02 +0300

    Animation on create and remove PostItem

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PostItem from './PostItem'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 
 // Destruct props to posts array on spot
 export default function PostList({posts, title, remove}) {
@@ -17,12 +18,23 @@ export default function PostList({posts, title, remove}) {
             <h1 style={{ textAlign: 'center' }}>
                 {title}
             </h1>
-            {
-                // Using of indexes as a key is a bad practice,
-                // because key must be static and unique,
-                // but indexes changes along with the array changes
-                posts.map((post, index) => <PostItem remove={remove} number={index + 1} post={post} key={post.id} />)
-            }
+            <TransitionGroup>
+                {
+                    // Key uses only on root element
+                    // Using of indexes as a key is a bad practice,
+                    // because key must be static and unique,
+                    // but indexes changes along with the array changes
+                    posts.map((post, index) => 
+                        <CSSTransition
+                            key={post.id}
+                            timeout={500}
+                            classNames="post"    
+                        >
+                            <PostItem remove={remove} number={index + 1} post={post}/>
+                        </CSSTransition>
+                    )
+                }
+            </TransitionGroup>
         </div>
     )
 }
